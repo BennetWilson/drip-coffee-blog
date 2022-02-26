174c1425e36a76512ee9ea13513bea50b053a120
Ian <new@news-MacBook-Pro.local>
2022-02-26 13:37:49 -0700

    added querys to client -> util -> queries.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,24 +1,23 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User , Post} = require("../models");
+const { User, Post } = require("../models");
 
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     getUsers: async () => {
-      return User.find().populate('posts');
+      return User.find().populate("posts");
     },
 
     getUser: async (parent, { username }) => {
-     
-      return User.findOne({username}).populate('posts')
+      return User.findOne({ username }).populate("posts");
     },
 
     posts: async () => {
       console.log("get post");
       return Post.find();
     },
-    
+
     me: async (parent, args, context) => {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id }).select(
@@ -54,23 +53,22 @@ const resolvers = {
     },
     addPost: async (parent, args, context) => {
       if (context.user) {
+        const post = await Post.create({
+          username: context.user.username,
+          title: args.title,
+          desc: args.desc,
+          photo: args.photo,
+        });
 
-        const post =  await Post.create({
-            username: context.user.username,
-            title: args.title,
-            desc: args.desc,
-            photo: args.photo
-        })
-
-       await User.findByIdAndUpdate(
+        await User.findByIdAndUpdate(
           { _id: context.user._id },
           { $addToSet: { posts: post._id } },
           { new: true }
         );
-        return post
+        return post;
       }
 
-        throw new AuthenticationError("Incorrect credentials");
+      throw new AuthenticationError("Incorrect credentials");
       //   let catArr = [];
 
       //   for (const category of categories) {
@@ -82,14 +80,48 @@ const resolvers = {
       //     desc,
       //     photo,
       //     $addToSet: { categories: catArr },
-     
+
       //   });
       //   return post;
+    },
+
+    // TODO check addComment and make sure its correct and relating to typeDefs
+    addComment: async (parent, { postId, commentText }, context) => {
+      if (context.user) {
+        return Post.findOneAndUpdate(
+          { _id: postId },
+          {
+            $addToSet: {
+              comments: { commentText, commentAuthor: context.user.username },
+            },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+
+    // TODO check removePost and make sure its correct and relating to typeDefs
+    removePost: async (parent, { postId }, context) => {
+      if (context.user) {
+        const post = await Post.findOneAndDelete({
+          _id: postId,
+          thoughtAuthor: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { posts: post._id } }
+        );
+
+        return post;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 };
 
 module.exports = resolvers;
-
-
-
