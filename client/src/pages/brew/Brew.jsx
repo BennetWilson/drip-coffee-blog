74787b90bf76d8607b8bea5747cd36f32be5e465
Jake Luedke <rajake18@gmail.com>
2022-02-27 10:27:02 -0700

    brew and recipe

---
+++ client/src/pages/brew/Brew.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './brew.css';
+
+import Header from '../../components/header/Header';
+import SideBar from '../../components/sidebar/Sidebar';
+import DripCover from '../../Assets/Drip-Cover.jpeg';
+
+export default function Brew() {
+    
+    return (
+        <div>
+            <Header />
+            <div className='brew'>
+                <div className='brewImg'>
+                    <img src={DripCover} alt="" className="brewImg" />
+                </div>
+                <div className='aboutSection'>
+                    <h3 className="brewTitle">Brewing Methods</h3>
+                    <SideBar />
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ client/src/pages/recipe/Recipe.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './recipe.css';
+
+import Header from '../../components/header/Header';
+import SideBar from '../../components/sidebar/Sidebar';
+import DripCover from '../../Assets/Drip-Cover.jpeg';
+
+export default function Recipe() {
+    
+    return (
+        <div>
+            <Header />
+            <div className='recipe'>
+                <div className='recipeImg'>
+                    <img src={DripCover} alt="" className="recipeImg" />
+                </div>
+                <div className='recipeSection'>
+                    <div className='recipeInfo'>
+                        <h3 className="recipeTitle">Coffee Cherry Seed Juice</h3>
+                        <p className="recipeP">
+                            With a global industy worth over $102 billion and employing 125 million people, it's safe to say 
+                            coffee is a craze that isn't going away any time soon. On average, there are 400 billion cups consumed 
+                            each year.
+
+                            Add a box to reveal a hidden answer to a question
+                            Question: Which country consumes the most coffee per capita?
+                            Answer: Finland at about 26lbs per year!
+                        </p>
+                        <ul className="recipesList">
+                            <li className='recipeItem'></li>
+                            <li className='recipeItem'></li>
+                            <li className='recipeItem'></li>
+                            <li className='recipeItem'></li>
+                            <li className='recipeItem'></li>
+                        </ul>
+                    </div>
+                    <SideBar />
+                </div>
+            </div>
+        </div>
+    );
