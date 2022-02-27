d35a5f3f9b5a95de168c9defa6a808b2a88159dc
Ian <new@news-MacBook-Pro.local>
2022-02-27 14:51:20 -0700

    update to brew.jsx

--- client/src/pages/brew/Brew.jsx
+++ client/src/pages/brew/Brew.jsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import './brew.css';
+import React, { useState } from "react";
+import "./brew.css";
+import { Card, Button, Col, Row } from "react-bootstrap";
 
-import Header from '../../components/header/Header';
-import SideBar from '../../components/sidebar/Sidebar';
-import DripCover from '../../Assets/Drip-Cover.jpeg';
+import Header from "../../components/header/Header";
+import SideBar from "../../components/sidebar/Sidebar";
+import DripCover from "../../Assets/Drip-Cover.jpeg";
 
 export default function Brew() {
-    
-    return (
-        <>
-        <Header />
-        <div>
-            <div className='brew'>
-                <div className='brewImg'>
-                    <img src={DripCover} alt="" className="brewImg" />
+  const [methods] = useState([
+    {
+      image: "",
+      methodName: "Cowboy Coffee",
+      methodType: "Boil",
+      equipment: "pot, open flame",
+      description:
+        "Unlike the name suggests, the cowboy coffee brew-method is used all over the world. It is one of the easiest methods as it doesn't require any special equipment outside of what most people already have in their kitchen. This method invovles boiling water, adding coffee grounds to the hot water, and pouring it into your mug of choice.",
+      steps:
+        "1. Boil the desired amount of water in the pot 2. When water reaches a boil take off heat for about 30 seconds or until the water reaches 200F 3. Add ~2 tbsps of ground coffee per 8 ounces of water and stir 4. Let stand for 2 mins and stir again 5. Let stand for another 2 mins then pour 1/2 cup of cold water onto floating grounds to help them sink 6. Pour into cup (if you're worried about grounds pour slowly)",
+      pros: "pros- cost effective, relatively easy, full flavor,full bodied",
+      cons: "cons - coffee grounds in cup, difficult to keep water temp consistent",
+      results: "simple and full bodied cup of joe",
+      recipes1: "",
+    },
+  ]);
+
+  return (
+    <>
+      <Header />
+      <div className="">
+        <div className="brew">
+          {methods.map((example, i) => (
+            <Card key={example.methodName} id={example.id} className="WorkCard">
+              <Col className="img-col">
+                <img
+                  src={require(`../../Assets/portfolio/${example.image}.png`)}
+                  alt="Img of portfolio project"
+                  className="project-image"
+                />
+              </Col>
+              <Card.Body>
+                <Col>
+                  <Card.Text className="description">
+                    {example.description}
+                    <br></br>
+                    {example.tech}
+                  </Card.Text>
+                </Col>
+                <div>
+                  <Button
+                    className="portBtn"
+                    href={example.link}
+                    traget="_blank"
+                  >
+                    {example.name}
+                  </Button>
+                  <Button
+                    className="portBtn"
+                    href={example.github}
+                    traget="_blank"
+                  >
+                    GitHub Repository
+                  </Button>
                 </div>
-                <div className='aboutSection'>
-                    <h3 className="brewTitle">Brewing Methods</h3>
-                    <SideBar />
-                </div>
-            </div>
+              </Card.Body>
+            </Card>
+          ))}
         </div>
-        </>
-    );
+      </div>
+      <SideBar />
+    </>
+  );
 }
